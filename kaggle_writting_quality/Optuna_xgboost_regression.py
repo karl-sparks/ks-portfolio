e0c6a8bd37be16abb7f327ac01788aa72159d28c
Karl Sparks <karl@sparks.to>
2023-11-04 13:31:33 +1100

    Some updates

--- kaggle_writting_quality/Optuna_xgboost_regression.py
+++ kaggle_writting_quality/Optuna_xgboost_regression.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
 import xgboost as xgb
 from xgboost import XGBRegressor
 import optuna
 import logging
+import lightgbm as lgb
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -17,10 +19,13 @@ class CONFIG:
     TEST_SIZE = 0.2
     ESTIMATORS = [("clf", XGBRegressor(random_state=RANDOM_SEED))]
     CV = 4
-    N_ITER = 200
+    N_ITER = 5000
+    PAUSE_THRESHOLD = 2000
 
 
 def clean_log_data(log_data):
+    logging.info("Cleaning log data")
+
     def num_unique_text(x):
         return len(set(x))
 
@@ -36,35 +41,64 @@ def clean_log_data(log_data):
     def third_quartile(series):
         return series.quantile(0.75)
 
+    def production_rate(x):
+        return (x.iloc[-1] - x.iloc[0]) * 60 / (x.index[-1] - x.index[0])
+
+    def pause_proportion(x):
+        return (x > CONFIG.PAUSE_THRESHOLD).sum() / len(x)
+
+    def revision_rate(x):
+        return (x.isin(["Remove/Cut", "Replace"])).sum() / len(x)
+
+    def movement_rate(x):
+        return x.str.startswith("Move From").sum() / len(x)
+
+    def paste_rate(x):
+        return (x == "Paste").sum() / len(x)
+
     agg_dict = {
         "event_id": "count",
-        "activity": [share_of_input, share_of_deletions],
-        "action_time": [
-            #       "mean",
-            #       "sum",
-            #       "std",
-            "min",
-            "max",
-            #       first_quartile,
-            #       third_quartile,
+        "activity": [
+            share_of_input,
+            share_of_deletions,
+            revision_rate,
+            movement_rate,
+            paste_rate,
         ],
-        #     "down_event": num_unique_text,
-        #      "up_event": num_unique_text,
-        "text_change": num_unique_text,
-        "word_count": [
-            #       "mean",
+        "action_time": [
+            "mean",
             "sum",
-            #       "std",
+            "std",
             "min",
             "max",
-            #       first_quartile,
+            first_quartile,
             third_quartile,
         ],
+        "down_event": num_unique_text,
+        "up_event": num_unique_text,
+        "text_change": num_unique_text,
+        "word_count": [
+            "mean",
+            "sum",
+            "std",
+            "min",
+            "max",
+            first_quartile,
+            third_quartile,
+            production_rate,
+        ],
+        "IKI": [pause_proportion, "mean", "sum", "std", "min", "max"],
     }
+
+    log_data["IKI"] = (
+        log_data.groupby("id")["down_time"].shift(-1) - log_data["up_time"]
+    )
 
     grouped_df = log_data.groupby("id").agg(agg_dict)
 
     grouped_df.columns = ["_".join(col).strip() for col in grouped_df.columns.values]
+
+    logging.info("Finished cleaning log data")
 
     return grouped_df
 
@@ -106,6 +140,12 @@ class objective:
             "subsample": trial.suggest_float("subsample", 0.2, 1.0),
             # sampling according to each tree.
             "colsample_bytree": trial.suggest_float("colsample_bytree", 0.2, 1.0),
+            "transform_y": trial.suggest_categorical(
+                "transform_y", ["identity", "minmax"]
+            ),
+            "model_type": trial.suggest_categorical(
+                "model_type", ["XGB", "classification"]
+            ),
         }
 
         if param["booster"] in ["gbtree", "dart"]:
@@ -130,9 +170,31 @@ class objective:
             param["rate_drop"] = trial.suggest_float("rate_drop", 1e-8, 1.0, log=True)
             param["skip_drop"] = trial.suggest_float("skip_drop", 1e-8, 1.0, log=True)
 
+        if param["transform_y"] == "minmax":
+            min_max_scaler = MinMaxScaler()
+
+            min_max_scaler.fit(self.target.values.reshape(-1, 1))
+
+            train_y = min_max_scaler.transform(train_y.values.reshape(-1, 1))
+
+        n_features = train_x.shape[1]
+
+        train_x_features = train_x.columns.tolist()
+
+        selected_features = [
+            trial.suggest_categorical(f"{train_x_features[i]}", [True, False])
+            for i in range(n_features)
+        ]
+
+        train_x_selected = train_x.iloc[:, selected_features]
+        valid_x_selected = valid_x.iloc[:, selected_features]
+
         bst = xgb.XGBRegressor(**param, random_state=CONFIG.RANDOM_SEED)
-        bst.fit(train_x, train_y)
-        preds = bst.predict(valid_x)
+        bst.fit(train_x_selected, train_y)
+        preds = bst.predict(valid_x_selected)
+
+        if param["transform_y"] == "minmax":
+            preds = min_max_scaler.inverse_transform(preds.reshape(-1, 1))
 
         root_mean_squared_error = np.sqrt(mean_squared_error(valid_y, preds))
         return root_mean_squared_error
@@ -158,11 +220,10 @@ if __name__ == "__main__":
             training_data.drop(columns=CONFIG.COLS_TO_DROP), training_data["score"]
         ),
         n_trials=CONFIG.N_ITER,
-        timeout=600,
+        timeout=6000,
     )
 
     print("Number of finished trials: ", len(study.trials))
-    print("Features: ", training_data.columns.tolist())
     print("Best trial:")
     trial = study.best_trial
 
